a1f307411a24d777b112f111ca51cb7e2fb3ff7c
COValhalla <josephvellella@proton.me>
2022-06-15 10:35:49 -0600

    Initial page grid and border styling.

---
+++ javascript.js
@@ -0,0 +1,24 @@
+// Tic tac toe, 3x3 board with two options for the player, X or O
+// Three of any type in a row wins
+
+// Gameboard object will store the details of the actual game
+// Gameboard grid will be stored as an array inside of a gameboard object
+const gameboard = (() => {
+    let gameArray = Array(8);
+}
+)();
+
+const displayController = (() => {
+    //X goes first.
+}
+)
+
+// Create CSS Grid with 3x3 layout, each grid position corresponds to gameboard grid.
+// Use event listener on the grid to select target square, if square already filled remove event listener
+
+
+
+
+//gameBoard/displayController will be modules, since only one is needed
+
+//Players will be created using factories (multiples needed)
