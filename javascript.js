26b2d24ad2e8306b381065d3bae4a26c87330b80
COValhalla <josephvellella@proton.me>
2022-06-18 09:40:14 -0600

    Updated javascript to correct for eslint suggestions.

--- javascript.js
+++ javascript.js
@@ -9,98 +9,7 @@ const Player = (name) => {
   return { getName, getPositions, updatePositions };
 };
 
-const gameboard = (() => {
-  const playerOne = Player('Player One');
-  const playerTwo = Player('Player Two');
-
-  const _gameArray = ['', '', '', '', '', '', '', '', ''];
-  const winConditions = [
-    '147',
-    '258',
-    '369',
-    '123',
-    '456',
-    '789',
-    '159',
-    '753',
-  ];
-
-  const init = () => {
-    addListener();
-    // Will initialize players in the future
-  };
-
-  const addListener = () => {
-    const blocks = document.querySelectorAll('.blocks');
-    blocks.forEach((block) => {
-      if (block.textContent == '') {
-        block.addEventListener(
-          'click',
-          () => {
-            roundLogic(block);
-          },
-          { once: true }
-        );
-      }
-    });
-  };
-
-  const roundLogic = (block) => {
-    const player = gameLogic.getPlayer();
-    updatePlayerObject(block);
-    checkWin(player);
-    updateGameArray(block);
-    drawGameBoard();
-  };
-
-  const updateGameArray = (block) => {
-    arrayID = block.id.slice(-1) - 1;
-
-    if (gameLogic.getTurn() == true) {
-      _gameArray[arrayID] = 'X';
-      gameLogic.updateTurn();
-    } else {
-      _gameArray[arrayID] = 'O';
-      gameLogic.updateTurn();
-    }
-  };
-
-  const drawGameBoard = () => {
-    _gameArray.forEach((element, index) => {
-      const block = document.getElementById(`block${index + 1}`);
-      block.textContent = element;
-    });
-  };
-
-  const updatePlayerObject = (block) => {
-    const blockID = block.id.slice(-1);
-    if (gameLogic.getTurn() == true) {
-      playerOne.updatePositions(blockID);
-    } else {
-      playerTwo.updatePositions(blockID);
-    }
-  };
-
-  const checkWin = (player) => {
-    if (player == 1) {
-      currentResults = playerOne.getPositions();
-    } else {
-      currentResults = playerTwo.getPositions();
-    }
-
-    winConditions.forEach((condition) => {
-      console.log(`Condition: ${condition}  Result: ${currentResults}`);
-    });
-  };
-
-  return {
-    init,
-    drawGameBoard,
-    updateGameArray,
-    updatePlayerObject,
-  };
-})();
-
+// Returns and updates turn. Returns current player.
 const gameLogic = (() => {
   let turnControl = true;
 
@@ -121,6 +30,100 @@ const gameLogic = (() => {
   };
 
   return { getTurn, updateTurn, getPlayer };
+})();
+
+const gameboard = (() => {
+  const playerOne = Player('Player One');
+  const playerTwo = Player('Player Two');
+
+  // eslint-disable-next-line no-underscore-dangle
+  const _gameArray = ['', '', '', '', '', '', '', '', ''];
+  const winConditions = [
+    '147',
+    '258',
+    '369',
+    '123',
+    '456',
+    '789',
+    '159',
+    '753',
+  ];
+
+  const updatePlayerObject = (block) => {
+    const blockID = block.id.slice(-1);
+    if (gameLogic.getTurn() === true) {
+      playerOne.updatePositions(blockID);
+    } else {
+      playerTwo.updatePositions(blockID);
+    }
+  };
+
+  const checkWin = (player) => {
+    let currentResults;
+    if (player === 1) {
+      currentResults = playerOne.getPositions();
+    } else {
+      currentResults = playerTwo.getPositions();
+    }
+
+    winConditions.forEach((condition) => {
+      console.log(`Condition: ${condition}  Result: ${currentResults}`);
+    });
+  };
+
+  const updateGameArray = (block) => {
+    const arrayID = block.id.slice(-1) - 1;
+
+    if (gameLogic.getTurn() === true) {
+      _gameArray[arrayID] = 'X';
+      gameLogic.updateTurn();
+    } else {
+      _gameArray[arrayID] = 'O';
+      gameLogic.updateTurn();
+    }
+  };
+
+  const drawGameBoard = () => {
+    _gameArray.forEach((element, index) => {
+      const block = document.getElementById(`block${index + 1}`);
+      block.textContent = element;
+    });
+  };
+
+  const roundLogic = (block) => {
+    const player = gameLogic.getPlayer();
+    updatePlayerObject(block);
+    checkWin(player);
+    updateGameArray(block);
+    drawGameBoard();
+  };
+
+  const addListener = () => {
+    const blocks = document.querySelectorAll('.blocks');
+    blocks.forEach((block) => {
+      if (block.textContent === '') {
+        block.addEventListener(
+          'click',
+          () => {
+            roundLogic(block);
+          },
+          { once: true },
+        );
+      }
+    });
+  };
+
+  const init = () => {
+    addListener();
+    // Will initialize players in the future
+  };
+
+  return {
+    init,
+    drawGameBoard,
+    updateGameArray,
+    updatePlayerObject,
+  };
 })();
 
 gameboard.init();
